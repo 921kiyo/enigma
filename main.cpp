440d93856028a9719a1afc3c070a2cd52320c9c4
Kiyohito Kunii <kk3317@voxel07.doc.ic.ac.uk>
2017-11-14 10:00:09 +0000

    add another endl

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
   cin >> message;
   for(int i = 0; message[i] != '\0'; i++){
     if(message[i] - 'A' < 0 || 25 < message[i] - 'A'){
-      cerr << message[i] << " is not a valid input character (input characters must be upper case letters A-Z)!";
+      cerr << message[i] << " is not a valid input character (input characters must be upper case letters A-Z)!" << endl;
       return INVALID_INPUT_CHARACTER;
     }
   }
