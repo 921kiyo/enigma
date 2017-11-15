35b4b1e2cb3823ac1dc17434ff5e5913aed34d9a
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-11-15 20:55:14 +0000

    add message cout

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv){
   // char encrypted_message[200];
 
   cin >> message;
-  // cout << "message " << message << endl;
+  cout << "message " << message << endl;
   for(int i = 0; message[i] != '\0'; i++){
     if(message[i] - 'A' < 0 || 25 < message[i] - 'A'){
       // cout << message[i] - 'A' << endl;
