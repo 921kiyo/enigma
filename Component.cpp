c5a3cd2294ab334118f4ab4ff8f3a44271a7a514
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-11-18 17:33:40 +0000

    add more comment for component class

--- Component.cpp
+++ Component.cpp
@@ -25,6 +25,8 @@ Component::Component(const char* path){
 }
 
 int Component::map(int input){
+  // Both pair1 and pair2 has to have the same length
+  // since they are in pair. So I use pair1 length.
   int length = input_pair1_.size();
   for(int i = 0; i< length; i++){
     if(input == input_pair1_[i]){
--- Component.h
+++ Component.h
@@ -7,13 +7,17 @@ class Component{
 private:
   // C++ 11 default value initialization
   static const int ALPHABET_LENGTH_{26};
+  // Store pair letters index in each vector, so it is easy to
+  // map because pair letters are stored at the same index.
   std::vector<int> input_pair1_;
   std::vector<int> input_pair2_;
 public:
   // Within this constructor, it gets all inputs from the file and assign them
   // to input_pair1_ and input_pair2_ vectors.
   Component(const char* path);
-
+  // Map the input accoding to wired-up pairs stored in
+  // input_pair1_ and input_pair2_ .
+  // Otherwise return input itself.
   int map(int input);
 };
 
