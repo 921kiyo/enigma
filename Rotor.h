ed9e591725018728e687f569bd39e14c09f8d535
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-11-18 17:48:57 +0000

    delete default value for current_position and previous_position

--- Rotor.h
+++ Rotor.h
@@ -7,11 +7,14 @@ using namespace std;
 
 class Rotor{
 private:
+  // C++ 11 default value initialization
   static const int ALPHABET_LENGTH_{26};
+  // TODO double check is this is necessary
+  int current_position_;
+  int previous_position_;
+
   int contacts_[ALPHABET_LENGTH_];
-  // Default value okay??
-  int current_position_{-1};
-  int previous_position_{-1};
+
   int num_of_notches_;
   vector<int> notches_;
 public:
