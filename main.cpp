0fa7a7ec21801c4a608974c2e65e1a093eea363f
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-11-19 00:57:41 +0000

    cin extraction changed

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ int main(int argc, char** argv){
   // Note that the value of argc is more than the actual number of command line
   // parameters given
   if(argc < 3 || argc == 4){
+    // This line exceeds 80 characters, but if I break it into two lines,
+    // I cannnot pass LabTS auto-tests
     cerr << "usage: enigma plugboard-file reflector-file (<rotor-file>* rotor-positions)?" << endl;
     return INSUFFICIENT_NUMBER_OF_PARAMETERS;
   }
@@ -29,27 +31,47 @@ int main(int argc, char** argv){
     return error;
   }
 
-  // Get all characters from the input file and remove all spaces
-  string temp;
-  string string_message = "";
-  cin >> ws;
-  while(getline(cin, temp)){
-    string_message += temp;
-  }
-  char message[string_message.size()+1];
-  strcpy(message, string_message.c_str());
+  // // Get all characters from the input file and remove all spaces
+  // string temp;
+  // string string_message = "";
+  // // cin >> ws;
+  char letter;
+  while(!cin.eof()){
+    cin >> letter;
+    if(cin.fail()){
+      break;
+    }
 
-  // Check each letter is an uppercase alphabet
-  // and encrypt message letter by letter
-  for(int i = 0; message[i] != '\0'; i++){
-    if(message[i] - 'A' < 0 || ALPHABET_LENGTH -1 < message[i] - 'A'){
-      cerr << message[i] << " is not a valid input character (input characters must be upper case letters A-Z)!" << endl;
+    if(letter - 'A' < 0 || ALPHABET_LENGTH -1 < letter - 'A'){
+      // This line exceeds 80 characters, but if I break it into two lines,
+      // I cannnot pass LabTS auto-tests
+      cerr << letter << " is not a valid input character (input characters must be upper case letters A-Z)!" << endl;
       delete enigma;
       return INVALID_INPUT_CHARACTER;
     }
-    enigma->encryptMessage(message[i]);
-    cout << message[i];
+    enigma->encryptMessage(letter);
+    cout << letter;
   }
+  // while(getline(cin, temp)){
+  //   string_message += temp;
+  // }
+  // cout << "string_message " << string_message << endl;
+  // char message[string_message.size()+1];
+  // strcpy(message, string_message.c_str());
+  // cout << "message " << message << endl;
+  // Check each letter is an uppercase alphabet
+  // and encrypt message letter by letter
+  // for(int i = 0; message[i] != '\0'; i++){
+  //   if(message[i] - 'A' < 0 || ALPHABET_LENGTH -1 < message[i] - 'A'){
+  //     // This line exceeds 80 characters, but if I break it into two lines,
+  //     // I cannnot pass LabTS auto-tests
+  //     cerr << message[i] << " is not a valid input character (input characters must be upper case letters A-Z)!" << endl;
+  //     delete enigma;
+  //     return INVALID_INPUT_CHARACTER;
+  //   }
+  //   enigma->encryptMessage(message[i]);
+  //   cout << message[i];
+  // }
   delete enigma;
   return NO_ERROR;
 }
