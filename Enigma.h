09bdc889ba3d1dad3ddbe8eaa0767b4665be8d80
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2017-11-20 01:03:53 +0000

    add lots of comment on Enigma class

--- Enigma.h
+++ Enigma.h
@@ -13,7 +13,7 @@ private:
 vector<Rotor> rotors_;
 vector<int> rotor_positions_;
 // Even though I could easily get the number of rotors from rotors_,
-// I sotore this as an attribute, because I use it frequently
+// I store this as an attribute, because I use it frequently
 // (e.g if statement, for loop etc).
 int num_of_rotors_;
 // C++ 11 default initialisation + use of nullptr
@@ -28,34 +28,28 @@ public:
   // exception is thrown while checking config, it throws an error to main
   // before a new heap is created in order to avoid unpredictable memory leak.
   Enigma(int argc, char** argv);
-
   // Free up plugboard and reflector heap in the destructor
   ~Enigma();
-
-  // These 4 methods check each input file by extracting input and check if
-  // each input is valid. At the same time they also assign each valid integer
-  // to corresponding vectors.
+  // These 4 checkConfig methods check each input file by extracting input
+  // and check if each input is valid (and throws exception error
+  // as defined in the spec).
+  // They also assign each valid integer to corresponding vectors.
   void checkPlugboardConfig(const char* path, vector<int>& contacts);
-
+  // This is a helper method for checking if inputs are valid for plugboard
+  // The reason this is in a separate method is that the numbers in plugboard
+  // have to be read off in pairs (as defined in the spec). Thus, unlike other
+  // checkConfig methods, I call istream twice in every while loop iteration.
   bool isPlugboardInputValid(const char* path, fstream& in_stream, int& index_num);
   void checkReflectorConfig(const char* path, vector<int>& contacts);
   void checkRotorConfig(const char* path, vector<int>& contacts);
   void checkRotorPositionConfig(const char* path);
-
-  // Check if num is within 0-25
+  // Check if num is within the range of 0-25
   bool isNumberRangeCorrect(int num);
-
-  // Check if mapping is valid
-  // (e.g duplicate contacts, a contact connected to itself etc.)
-  // bool checkMapping(vector<int> contacts, int range);
-
   // Check if num is already appeared in contacts.
   // If yes, return the index of the previous contact. Otherwise return -1
   int checkAppearedBefore(vector<int> contacts, int num, int position);
-  // int checkAppearedBefore2(vector<int> contacts, int num, int position);
-
   // This method does whole encryption process
-  // and convert letter into encrypted one.
+  // and convert a letter into encrypted one.
   void encryptMessage(char& letter);
 };
 
